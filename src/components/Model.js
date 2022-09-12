7326b402f7bb123bde0f7e4fe2a7027c9ca9d99a
Soham <sohamdeb0745@gmail.com>
2022-09-12 19:52:55 +0530

    Initial commit

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,23 +1,26 @@
-import React, { Suspense, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useState ,useRef} from 'react';
+import { Canvas,useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-
 import Moon from './Moon';
 import Menu from './Menu';
-
 import '../styles/Model.scss';
 
 function Model() {
   const [axes, setAxes] = useState(10);
-
+  
   return (
     <>
       <div className='model'>
         <Canvas className='canvas' camera={{ fov: 35, zoom: 1, near: 1, far: 1000 }}>
-          <OrbitControls enableZoom={false} />
+        <OrbitControls 
+          enableRotate={true}
+            enableZoom={false} />
+            
           <Suspense>
+            
             <ambientLight intensity={0.03} />
             <spotLight position={[10, 0, 10]} intensity={1} angle={0.5} />
+            
             <Moon />
             <axesHelper args={[axes, 50, 50]} position={[0, 0, 0]} />
           </Suspense>
@@ -27,5 +30,6 @@ function Model() {
     </>
   );
 }
+
 
 export default Model;
--- src/components/Moon.js
+++ src/components/Moon.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { useLoader } from '@react-three/fiber';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
 import * as THREE from 'three';
 import moon from '../assets/moon.jpg';
+import { useState } from 'react';
+import { useRef } from 'react';
+import gsap from 'gsap';
+
 
 function Moon() {
   const moonTexture = useLoader(THREE.TextureLoader, moon);
+  const sphereRef = useRef()
+  const groupRef =useRef()
+  
+  const [cursor,changeCursor]=useState({
+    x:0,
+    y:0
+  })
+  document.addEventListener("mousemove",(e)=>{
+    changeCursor({
+      x:(e.clientX/window.innerWidth)*2-1,
+      y:(e.clientY/window.innerHeight)*2+1
+    })
+  })
+  useFrame(()=>{
+    sphereRef.current.rotation.y+=0.0005
+    gsap.to(
+      groupRef.current.rotation,
+      {
+        x:-cursor.y*0.06,
+        y:cursor.x*0.06,
+        duration:2
+      }
+    )
+    
+  })
 
   return (
     <>
-      <Stars radius={300} depth={60} count={20000} factor={7} saturation={0} fade={true} />
-      <mesh rotation={[0, -Math.PI / 5, 0]}>
+      <Stars radius={300} depth={60} count={204000} factor={7} saturation={0} fade={true} />
+      <group ref={groupRef}>
+      <mesh ref={sphereRef} rotation={[0, -Math.PI/5, 0]}>
         <sphereGeometry attach='geometry' args={[1, 100, 100]} />
         <meshPhongMaterial color='black' />
         <meshStandardMaterial map={moonTexture} />
       </mesh>
+      </group>
     </>
   );
 }
